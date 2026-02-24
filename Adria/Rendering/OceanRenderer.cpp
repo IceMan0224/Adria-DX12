39133bb84ef8afb3a5cd35f50f0413e57e303016
Mate Buljan <mate.buljan1@gmail.com>
2026-02-24 22:19:03 +0000

    fix ocean rendering on mac

--- Adria/Rendering/OceanRenderer.cpp
+++ Adria/Rendering/OceanRenderer.cpp
@@ -433,6 +433,7 @@ namespace adria
 		GfxGraphicsPipelineStateDesc gfx_pso_desc{};
 		gfx_pso_desc.input_layout.elements.push_back({"POSITION", 0, GfxFormat::R32G32B32_FLOAT, 0, 0, GfxInputClassification::PerVertexData});
 		gfx_pso_desc.input_layout.elements.push_back({"TEX", 0, GfxFormat::R32G32_FLOAT, 0, 12, GfxInputClassification::PerVertexData});
+		gfx_pso_desc.input_layout.elements.push_back({"NORMAL", 0, GfxFormat::R32G32B32_FLOAT, 0, 20, GfxInputClassification::PerVertexData});
 		gfx_pso_desc.root_signature = GfxRootSignatureID::Common;
 		gfx_pso_desc.VS = VS_Ocean;
 		gfx_pso_desc.PS = PS_Ocean;
